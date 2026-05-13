5adf76013515b8c91e386316a849fe6c0755ef48
DanieJGomez <gomezdanieljose75@gmail.com>
2026-05-12 20:04:02 -0500

    Agregue el borrador de la estructura de la cita

---
+++ Agenda.cpp
@@ -0,0 +1,21 @@
+/*
+ PROYECTO 7 - AGENDA PERSONAL  Programacion Basica - Universidad Distrital F.J. de Caldas
+ Autores : Daniel Gomez, Nicolas Pulido, Hollman Castillo
+ Fecha : 2/06/2024
+ agenda digital que permite registrar, consultar, modificar y eliminar citas o tareas pendientes,
+ organizadas por fecha y hora. en (agenda.txt) */
+ 
+#include <iostream> // para entrada y salida de datos de cin y cout
+using namespace std; 
+
+
+//  ESTRUCTURA: Cita
+struct Cita {
+    string fecha;        // DD/MM/AAAA
+    string hora;         // HH:MM
+    string descripcion;  // Max. 100 caracteres
+};
+int main() {
+    cout << "Agendita";
+    return 0;
+}
